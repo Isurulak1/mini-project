4cabfa8ef99eed7737869dd7d1901a4aa9a58798
Isurulak1 <isurulakshan0417@gmail.com>
2024-12-17 09:29:23 +0530

    finalized

--- src/firebase.js
+++ src/firebase.js
@@ -5,12 +5,7 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBXBZ7iPEV4hHCP2xib44ul70XTLvk-AUk",
-  authDomain: "mini-project-65fbd.firebaseapp.com",
-  projectId: "mini-project-65fbd",
-  storageBucket: "mini-project-65fbd.appspot.com",
-  messagingSenderId: "1050105289090",
-  appId: "1:1050105289090:web:eb2beac5837a8b1b1ed79b"                           
+                             
 };
 
 // Initialize Firebase
